b6dbbe9af1f4442d53287bd8725a22c1b019b0db
James Juchau <jhjuchau@gmail.com>
2019-11-09 13:10:05 -0800

    Fixing eof loop

--- main.c
+++ main.c
@@ -5,41 +5,60 @@
 #include "queue.h"
 #include "sj2_cli.h"
 #include "task.h"
+
+#include "ff.h"
+#include "sl_string.h"
+
 #include <stdio.h>
 
-
-
-typedef struct File {
+typedef struct File_contents {
   char songname[32];
+} File_list;
 
-} songname;
-
+File_list file;
 QueueHandle_t Q_songname;
 QueueHandle_t Q_songdata;
 
 void main(void) {
-  Q_songname = xQueueCreate(sizeof(songname), 1);
+  Q_songname = xQueueCreate(sizeof(char[32]), 1);
   Q_songdata = xQueueCreate(512, 1);
+}
+
+char fpeek(FILE *stream) {
+  char c;
+
+  c = fgetc(stream);
+  ungetc(c, stream);
+
+  return c;
 }
 
 // Reader tasks receives song-name over Q_songname to start reading it
 void mp3_reader_task(void *p) {
-
-  songname name;
+  char name[32];
   char bytes_512[512];
-  
 
   while (1) {
-    xQueueReceive(Q_songname, &name[0], portMAX_DELAY);
+    xQueueReceive(Q_songname, &name, portMAX_DELAY);
     printf("Received song to play: %s\n", name);
 
-    open_file();
-    while (!file.end()) {
+    FILE *mp3file; // File handle
+    FRESULT result = f_open(&file, name, (FA_READ));
+
+    char c = fpeek(mp3file);
+
+    while (c != EOF) {
       read_from_file(bytes_512);
       xQueueSend(Q_songdata, &bytes_512[0], portMAX_DELAY);
+      char c = fpeek(mp3file);
     }
     close_file();
   }
+}
+
+void find_mp3_by_filename(char name[32]) {
+  FIL file; // File handle
+  FRESULT result = f_open(&file, name, (FA_READ));
 }
 
 // Player task receives song data over Q_songdata to send it to the MP3 decoder
